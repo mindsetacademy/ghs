b7964df03d3a6027ef452cb6116313f22a858150
mindsetacademy <146858945+mindsetacademy@users.noreply.github.com>
2023-10-05 09:24:12 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,49 @@
+
+<!-- warning -->
+<div class="bloc b-parallax bloc-bg-texture texture-darken tc-3498 l-bloc" id="warning">
+	<div class="parallax bg-iphone-os-wall">
+	</div>
+	<div class="bloc-shape-divider bloc-divider-bottom">
+		<svg class="svg-divider bloc-divider-b-warning-style" fill-rule="evenodd" preserveAspectRatio="none" viewBox="0 0 1000 250"><path d="M0,125H1000V250H0V125Z"></path></svg>
+	</div>
+	<div class="container bloc-lg-lg bloc-lg">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2">
+					<h2 class="mg-md text-center tc-5996">
+						iPhone
+					</h2>
+					<h4 class="mg-md text-center tc-5996">
+						Mockup of Apple’s iPhone 12 Pro website
+					</h4>
+					<p class="text-center mg-md">
+						All content is a property of Apple. This page was recreated in <a class="apple-white-link" href="https://eldargezalov.com/get-blocs" target="_blank">Blocs for Mac</a> to showcase capabilities of this web design app. If you have <a class="apple-white-link" href="https://blocstemplates.com" target="_blank">Blocs 4 Templates</a>, you can download this project file (for educational purposes only).
+					</p>
+					<p class="text-center mg-lg">
+						<a class="apple-blue-link apple-bold" href="https://blocstemplates.com" target="_blank">Blocs Templates ></a>&nbsp; &nbsp;&nbsp; <a class="apple-blue-link apple-bold" href="https://blocsmaster.com" target="_blank">Blocs Master ></a>
+					</p>
+				</div>
+			</div>
+			<div class="row voffset-md">
+				<div class="col">
+					<p class="text-center mg-clear">
+						Recreated by <a class="apple-white-link" href="https://eldargezalov.com" target="_blank">Eldar Gezalov</a>
+					</p>
+				</div>
+			</div>
+		</div>
+</div>
+<!-- warning END -->
+
+<!-- bloc-30 -->
+<div class="bloc l-bloc" id="bloc-30">
+	<div class="container bloc-no-padding">
+		<div class="row">
+			<div class="col">
+				<div><div id="page-transition-anim" class="page-transition page-trans-fade" style="background-color: rgb(0, 0, 0); animation-duration: 1s; animation-delay: 1s; background-position: initial initial; background-repeat: initial initial;">
+</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-30 END -->
---
+++ header.php
@@ -0,0 +1,98 @@
+
+<!-- start -->
+<div class="bloc bgc-6653 tc-3498 bg-iphone-os-wall d-bloc " id="start">
+	<div class="container bloc-sm-lg bloc-sm-md bloc-sm-sm bloc-sm">
+		<div class="row">
+			<div class="align-self-center col-lg-11 col-md-11 col-sm-12">
+				<h5 class="text-center tc-5996 mg-clear text-lg-center mg-sm-xs mg-sm-sm">
+					This webpage is a mockup of Apple website created in <a class="apple-white-link" href="https://eldargezalov.com/get-blocs" target="_blank">Blocs 4</a>.
+				</h5>
+			</div>
+			<div class="align-self-center col-md-1">
+				<div class="text-center">
+					<a href="#" class="btn btn-d apple-blue-wire-button btn-rd btn-sm" data-toggle-class-target="start" data-toggle-class="apple-start">Hide</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- start END -->
+
+<!-- bloc-1 -->
+<div class="bloc bgc-928 d-bloc" id="bloc-1">
+	<div class="container bloc-no-padding-lg bloc-no-padding">
+		<div class="row">
+			<div class="col-lg-1 align-self-center col-4 order-2 order-lg-1">
+				<a href="<?php echo home_url('/'); ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/apple.svg" class="img-fluid img-protected mx-auto d-block apple-navbar-logo apple-main-logo" alt="apple" width="16" height="44"></a>
+			</div>
+			<div class="col-lg-10 align-self-center col-4 order-1">
+				<nav class="navbar row navbar-dark navbar-expand-lg apple-navbar-toggle" role="navigation">
+					<button id="nav-toggle" type="button" class="ml-auto ui-navbar-toggler navbar-toggler border-0 p-0" data-toggle="collapse" data-target=".navbar-30648" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"><svg height="32" viewBox="0 0 32 32" width="32"><path class="svg-menu-icon menu-icon-two-bars" d="m2 10h28m-28 12h28"></path></svg></span>
+					</button>
+					<div class="collapse navbar-collapse navbar-30648  fullscreen-nav open-down">
+							<ul class="site-navigation nav navbar-nav ml-auto apple-nav-list">
+								<li class="nav-item">
+									<a href="<?php echo home_url('/'); ?>" class="nav-link a-btn apple-nav-link">Mac</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?php echo home_url('/'); ?>" class="nav-link a-btn apple-nav-link">iPad</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?php echo home_url('/'); ?>" class="nav-link a-btn apple-nav-link">iPhone</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?php echo home_url('/'); ?>" class="nav-link a-btn apple-nav-link">Watch</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?php echo home_url('/'); ?>" class="nav-link a-btn apple-nav-link">TV</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?php echo home_url('/'); ?>" class="nav-link a-btn apple-nav-link">Music</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?php echo home_url('/'); ?>" class="nav-link a-btn apple-nav-link">Soporte</a>
+								</li>
+							</ul>
+						</div>
+				</nav>
+			</div>
+			<div class="col-lg-1 align-self-center col-4 order-2">
+					<a href="https://blocstemplates.com/iphone.html" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/img/bag.svg" class="img-fluid img-protected float-lg-right float-md-right float-sm-right float-right apple-navbar-logo" alt="bag" width="16" height="45"></a>
+				</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-1 END -->
+
+<!-- bloc-1 -->
+<div class="bloc apple-subnav d-bloc sticky-nav" id="bloc-1">
+	<div class="container bloc-no-padding">
+		<div class="row">
+			<div class="col">
+				<nav class="navbar row navbar-expand-md navbar-dark apple-mobile-nav" role="navigation">
+					<a class="navbar-brand apple-subnav-logo" href="<?php echo home_url('/'); ?>">iPhone &nbsp;12 Pro</a>
+					<button id="nav-toggle" type="button" class="ml-auto ui-navbar-toggler navbar-toggler border-0 p-0" data-toggle="collapse" data-target=".navbar-40900" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"><svg height="32" viewBox="0 0 32 32" width="32"><path class="svg-menu-icon menu-icon-filled menu-icon-squares" d="m2 14h6v6h-6zm11 0h6v6h-6zm11 0h6v6h-6z"></path></svg></span>
+					</button>
+					<div class="collapse navbar-collapse navbar-40900  sidebar-nav">
+							<ul class="site-navigation nav navbar-nav ml-auto">
+								<li class="nav-item">
+									<a href="<?php echo home_url('/'); ?>" class="nav-link apple-subnav-link apple-grey">Descripción</a>
+								</li>
+								<li class="nav-item">
+									<a href="<?php echo home_url('/'); ?>" class="nav-link apple-subnav-link">Especificaciones</a>
+								</li>
+							</ul>
+						</div>
+					<div class="navbar-content-area col-lg-1 col-md-2 col-sm-2 col-4 apple-mobile-nav">
+							<div class="text-center">
+								<a href="https://blocstemplates.com/iphone.html" class="btn btn-rd btn-sm apple-blue-button btn-d" target="_blank">Compar</a>
+							</div>
+						</div>
+				</nav>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-1 END -->
